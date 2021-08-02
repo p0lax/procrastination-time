f862ef0271d9eeb381492ac80c182c4330148dc1
p0lax <polax@list.ru>
2021-08-02 21:22:29 +0200

    More sounds

--- src/sections/constants.ts
+++ src/sections/constants.ts
@@ -4,17 +4,23 @@ import train from './../assets/images/train_0.jpg';
 import forest from './../assets/images/forest_0.jpg';
 import winter from './../assets/images/winter_0.jpg';
 import road from './../assets/images/road_0.jpg';
-import wetRoad from './../assets/images/wet_road_0.jpg';
+import wetRoad from './../assets/images/wet_road_1.jpg';
 import dinner from './../assets/images/dinner_0.jpg';
+import trainStation from './../assets/images/train_station_0.jpg';
+import sea from './../assets/images/sea_0.jpg';
+import village from './../assets/images/village_1.jpg';
 
 import rainSound from 'url:./../assets/sounds/rain_0.mp3';
 import trainSound from 'url:./../assets/sounds/train_0.mp3';
 import morningSound from 'url:./../assets/sounds/morning_0.mp3';
-import forestSound from 'url:./../assets/sounds/forest_1.mp3';
+import forestSound from 'url:./../assets/sounds/forest_0.mp3';
 import winterSound from 'url:./../assets/sounds/winter_0.mp3';
 import roadSound from 'url:./../assets/sounds/road_0.mp3';
 import wetRoadSound from 'url:./../assets/sounds/wet_road_0.mp3';
 import dinnerSound from 'url:./../assets/sounds/dinner_0.mp3';
+import trainStationSound from 'url:./../assets/sounds/train_station_0.mp3';
+import seaSound from 'url:./../assets/sounds/sea_0.mp3';
+import villageSound from 'url:./../assets/sounds/village_0.mp3';
 
 export const CARDS = [
   {
@@ -60,6 +66,20 @@ export const CARDS = [
     sound: roadSound,
   },
   {
+    id: 'sea',
+    title: 'Sea beach',
+    description: 'Sounds of the Caribean sea',
+    img: sea,
+    sound: seaSound,
+  },
+  {
+    id: 'village',
+    title: 'Evening in the village',
+    description: 'Sounds of the evening village',
+    img: village,
+    sound: villageSound,
+  },
+  {
     id: 'wet_road',
     title: 'Road after heavy rain',
     description: 'Sounds of the the cars on the road after a heavy rain',
@@ -72,5 +92,12 @@ export const CARDS = [
     description: 'Sounds of the the restaurant',
     img: dinner,
     sound: dinnerSound,
+  },
+  {
+    id: 'train_station',
+    title: 'Saint-Petersburg trains station',
+    description: 'Sounds of the Saint-Petersburg train station',
+    img: trainStation,
+    sound: trainStationSound,
   },
 ];
